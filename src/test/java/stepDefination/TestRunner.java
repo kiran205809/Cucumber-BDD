3245126974b6ca9a47e9656d0da8ca1679987b74
Reset@132 <kiranx.ananthram@intel.com>
2022-12-18 12:43:45 +0530

    commit

--- src/test/java/stepDefination/TestRunner.java
+++ src/test/java/stepDefination/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		
 		
-		plugin = {"html:target/html-cucumber"},
+		plugin = {"html:target/html-cucumber","json:target/cucumber.json"},
 		
 		features = "src/test/java/features",
 		//tags = {"@Sanity, @Regression"},
